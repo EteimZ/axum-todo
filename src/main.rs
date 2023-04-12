6a4699b3f36194ed302472a4be4624fa737b2384
eteimz <8teims@gmail.com>
2023-04-12 10:48:02 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,197 @@
+use axum::{
+    error_handling::HandleErrorLayer,
+    extract::{Path, Query, State},
+    http::StatusCode,
+    routing::{get, patch},
+    Json, Router, response::IntoResponse,
+};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
+
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "axum_todo=debug,tower_http=debug".into(),)
+            )
+            .with(tracing_subscriber::fmt::layer())
+            .init();
+    
+    // Set the the initial value of the database
+    let db = Db::default();
+    
+    // compose the routes
+    let app = Router::new()
+        .route("/todos", get(todos_index).post(todos_create))
+        .route("/todos/:id", patch(todos_update).delete(todos_delete).get(todos_get))
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>(){
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {}", error),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .into_inner()
+        )
+        .with_state(db);
+    
+     // add a fallback service for handling routes to unknown paths
+     let app = app.fallback(handler_404);
+
+    // set the socket address
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    
+    // create the server
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// set up the database
+type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
+
+// struct that defines todo
+#[derive(Debug, Serialize, Clone)]
+struct Todo{
+    id: Uuid,
+    text: String,
+    completed: bool,
+}
+
+// define struct for query parameters
+#[derive(Debug, Deserialize, Default)]
+pub struct Pagination {
+    pub offset: Option<usize>,
+    pub limit: Option<usize>,
+}
+
+// route to get all todos
+async fn todos_index(
+    pagination: Option<Query<Pagination>>,
+    State(db): State<Db>
+) -> impl IntoResponse {
+    let todos = db.read().unwrap();
+
+    let Query(pagination) = pagination.unwrap_or_default();
+    
+    let todos = todos
+        .values()
+        .skip(pagination.offset.unwrap_or(0))
+        .take(pagination.limit.unwrap_or(usize::MAX))
+        .cloned()
+        .collect::<Vec<_>>();
+
+    Json(todos)
+}
+
+// define a struct to create todo 
+#[derive(Debug, Deserialize)]
+struct CreateTodo{
+    text: String,
+}
+
+// create todo route using CreateTodo struct as the body
+async fn todos_create(State(db): State<Db>, Json(input): Json<CreateTodo>) -> impl IntoResponse {
+    let todo = Todo {
+        id: Uuid::new_v4(),
+        text: input.text,
+        completed: false
+    };
+
+    db.write().unwrap().insert(todo.id, todo.clone());
+
+    (StatusCode::CREATED, Json(todo))
+}
+
+// define a struct to update todo 
+#[derive(Debug, Deserialize)]
+struct UpdateTodo {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+// update todo route using UpdateTodo struct as the body
+async fn todos_update(
+    Path(id): Path<Uuid>,
+    State(db): State<Db>,
+    Json(input): Json<UpdateTodo>
+) -> Result<impl IntoResponse, StatusCode>
+{
+    let mut todo = db
+        .read()
+        .unwrap()
+        .get(&id)
+        .cloned()
+        .ok_or(StatusCode::NOT_FOUND)?;
+
+    if let Some(text) = input.text{
+        todo.text = text;
+    }
+
+    if let Some(completed) = input.completed{
+        todo.completed = completed
+    }
+
+    db.write().unwrap().insert(todo.id, todo.clone());
+
+    Ok(Json(todo))
+}
+
+// route to get a particular todo
+async fn todos_get(
+    Path(id): Path<Uuid>,
+    State(db): State<Db>,
+) -> Result<impl IntoResponse, StatusCode>
+{
+    let todo = db
+        .read()
+        .unwrap()
+        .get(&id)
+        .cloned()
+        .ok_or(StatusCode::NOT_FOUND)?;
+    
+    Ok(Json(todo))
+}
+
+// route to delete a particular todo
+async fn todos_delete(Path(id): Path<Uuid>, State(db): State<Db>) -> impl IntoResponse {
+    if db.write().unwrap().remove(&id).is_some(){
+        StatusCode::NO_CONTENT
+    } else {
+        StatusCode::NOT_FOUND
+    }
+} 
+
+
+// 404 route
+async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, Json(NotFoundResponse{ detail: String::from("Endpoint not found")}))
+}
+
+// response struct from 404 route
+#[derive(Serialize)]
+struct NotFoundResponse {
+    detail: String
+}
+
